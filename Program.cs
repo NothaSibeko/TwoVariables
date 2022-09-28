ec487f0bdeb487eebf98d8ce9594d90d5f6ea66a
notha <notha@LAPTOP-N9HV6Q5U>
2022-09-28 20:16:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Notha ";
+            string lastName = "Sibeko";
+            string fullName = firstName + lastName;
+            Console.WriteLine("Hello I am " + fullName);
+        }
+    }
+}
